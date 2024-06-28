bb798dccb96c62c0c9791757a20700ee68f69716
benwstaab <136244725+benwstaab@users.noreply.github.com>
2024-06-28 09:37:51 +0800

    Update api.js

--- static/js/api.js
+++ static/js/api.js
@@ -13,7 +13,7 @@ function submit() {
     }
 
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", `https://cute-shalna-greanpeakcharger-aa7d6c68.koyeb.app/`);
+    xhr.open("GET", `classic-gerianne-greanpeakcharger-54019e82.koyeb.app/`);
     xhr.responseType = "json";
 
     xhr.onload = () => {
